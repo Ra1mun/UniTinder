e2cd08c73cad3afa1c11951d7d69212e5622fe0a
Anigilator322 <Anigilator322@gmail.com>
2024-03-15 23:59:47 +0700

    Chat window

--- Assets/UniTinder/UI/Realisation/UIChatWindow/UIChatWindow.cs
+++ Assets/UniTinder/UI/Realisation/UIChatWindow/UIChatWindow.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TMPro;
 using UniTinder.UI.UIService;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,14 +9,19 @@ namespace UniTinder.UI.Realisation
     public class UIChatWindow : UIWindow
     {
         public Action GoToPreviousWindowEvent;
+        public Action SendMessageEvent;
         
+        [SerializeField] private Button sendMessage;
         [SerializeField] private Button previousButton;
-        
         public override void Show()
         {
             previousButton.onClick.AddListener(GoToPreviousButtonClick);
+            sendMessage.onClick.AddListener(SendMessage);
         }
-
+        private void SendMessage()
+        {
+            SendMessageEvent?.Invoke();
+        }
         private void GoToPreviousButtonClick()
         {
             GoToPreviousWindowEvent?.Invoke();
@@ -24,6 +30,10 @@ namespace UniTinder.UI.Realisation
         public override void Hide()
         {
             previousButton.onClick.RemoveListener(GoToPreviousButtonClick);
+        }
+        public void AddMessage(MessageView view)
+        {
+            view.transform.SetParent(transform);
         }
     }
 }
